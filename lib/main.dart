5eaf98c4b7a5faffc64a8c68e47b26ccc7e78bcd
luistrivelatto <luigi.nys@gmail.com>
2018-04-14 03:18:42 -0300

    Split SortableListView behaviour into new widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,45 +14,6 @@ class MyAppState extends State<MyApp> {
     ..add('Row 3')
     ..add('Row 4');
 
-  void _handleAccept(int data, int index) {
-    setState(() {
-      // Decrement index so that after removing we'll still insert the item
-      // in the correct position.
-      if (index > data) {
-        index--;
-      }
-      String imageToMove = rows[data];
-      rows.removeAt(data);
-      rows.insert(index, imageToMove);
-    });
-  }
-
-  Widget _getListItem(int index, [double elevation = 2.0]) {
-    // A little hack: our ListView has an extra invisible trailling item to
-    // allow moving the dragged item to the last position.
-    if (index == rows.length) {
-      // This invisible item uses the previous item to determine its size. If
-      // the list is empty, though, there's no dragging really.
-      if (rows.isEmpty) {
-        return new Container();
-      }
-      return new Opacity(
-        opacity: 0.0,
-        child: _getListItem(index - 1),
-      );
-    }
-
-    return new Card(
-      child: new Column(
-        children: <Widget>[
-          new ListTile(
-              leading: new Icon(Icons.photo), title: new Text(rows[index])),
-        ],
-      ),
-      elevation: elevation,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final title = 'Sortable ListView';
@@ -63,64 +24,124 @@ class MyAppState extends State<MyApp> {
         appBar: new AppBar(
           title: new Text(title),
         ),
-        body: new LayoutBuilder(builder: (context, constraint) {
-          return new ListView.builder(
-            itemCount: rows.length + 1,
-            addRepaintBoundaries: true,
-            itemBuilder: (context, index) {
-              return new LongPressDraggable<int>(
-                data: index,
-                child: new DragTarget<int>(
-                  onAccept: (int data) {
-                    _handleAccept(data, index);
-                  },
-                  builder: (BuildContext context, List<int> data,
-                      List<dynamic> rejects) {
-                    List<Widget> children = [];
-
-                    // If the dragged item is on top of this item, the we draw
-                    // a half-visible item to indicate that dropping the dragged
-                    // item will add it in this position.
-                    if (data.isNotEmpty) {
-                      children.add(
-                        new Container(
-                          decoration: new BoxDecoration(
-                            border: new Border.all(
-                                color: Colors.grey[600], width: 2.0),
-                          ),
-                          child: new Opacity(
-                            opacity: 0.5,
-                            child: _getListItem(data[0]),
-                          ),
-                        ),
-                      );
-                    }
-                    children.add(_getListItem(index));
-
-                    return new Column(
-                      children: children,
-                    );
-                  },
-                ),
-                onDragStarted: () {
-                  Scaffold.of(context).showSnackBar(
-                        new SnackBar(
-                            content: new Text("Drag the row to change places")),
-                      );
-                },
-                feedback: new Opacity(
-                  opacity: 0.75,
-                  child: new SizedBox(
-                    width: constraint.maxWidth,
-                    child: _getListItem(index, 18.0),
-                  ),
-                ),
-                childWhenDragging: new Container(),
-              );
-            },
-          );
-        }),
+        body: new SortableListView(
+          rows,
+          (_, int index) => new Card(
+                child: new ListTile(
+                    leading: new Icon(Icons.photo),
+                    title: new Text(rows[index])),
+              ),
+        ),
       ),
+    );
+  }
+}
+
+class SortableListView extends StatefulWidget {
+  final List items;
+  final IndexedWidgetBuilder itemBuilder;
+
+  SortableListView(this.items, this.itemBuilder);
+
+  @override
+  State createState() => new SortableListViewState();
+}
+
+class SortableListViewState extends State<SortableListView> {
+  @override
+  Widget build(BuildContext context) {
+    return new LayoutBuilder(
+      builder: (context, constraint) {
+        return new ListView.builder(
+          itemCount: widget.items.length + 1,
+          addRepaintBoundaries: true,
+          itemBuilder: (context, index) {
+            return new LongPressDraggable<int>(
+              data: index,
+              child: new DragTarget<int>(
+                onAccept: (int data) {
+                  _handleAccept(data, index);
+                },
+                builder: (BuildContext context, List<int> data,
+                    List<dynamic> rejects) {
+                  List<Widget> children = [];
+
+                  // If the dragged item is on top of this item, the we draw
+                  // a half-visible item to indicate that dropping the dragged
+                  // item will add it in this position.
+                  if (data.isNotEmpty) {
+                    children.add(
+                      new Container(
+                        decoration: new BoxDecoration(
+                          border: new Border.all(
+                              color: Colors.grey[600], width: 2.0),
+                        ),
+                        child: new Opacity(
+                          opacity: 0.5,
+                          child: _getListItem(context, data[0]),
+                        ),
+                      ),
+                    );
+                  }
+                  children.add(_getListItem(context, index));
+
+                  return new Column(
+                    children: children,
+                  );
+                },
+              ),
+              onDragStarted: () {
+                Scaffold.of(context).showSnackBar(
+                      new SnackBar(
+                          content: new Text("Drag the row to change places")),
+                    );
+              },
+              feedback: new Opacity(
+                opacity: 0.75,
+                child: new SizedBox(
+                  width: constraint.maxWidth,
+                  child: _getListItem(context, index, true),
+                ),
+              ),
+              childWhenDragging: new Container(),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _handleAccept(int data, int index) {
+    setState(() {
+      // Decrement index so that after removing we'll still insert the item
+      // in the correct position.
+      if (index > data) {
+        index--;
+      }
+      String imageToMove = widget.items[data];
+      widget.items.removeAt(data);
+      widget.items.insert(index, imageToMove);
+    });
+  }
+
+  Widget _getListItem(BuildContext context, int index, [bool dragged]) {
+    // A little hack: our ListView has an extra invisible trailing item to
+    // allow moving the dragged item to the last position.
+    if (index == widget.items.length) {
+      // This invisible item uses the previous item to determine its size. If
+      // the list is empty, though, there's no dragging really.
+      if (widget.items.isEmpty) {
+        return new Container();
+      }
+      return new Opacity(
+        opacity: 0.0,
+        child: _getListItem(context, index - 1),
+      );
+    }
+
+    return new Material(
+      elevation: dragged ? 20.0 : 0.0,
+      child: widget.itemBuilder(context, index),
     );
   }
 }
